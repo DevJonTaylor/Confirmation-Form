7011619b2e748e76875d78f1535e083c8e82e027
jontaylor <jonnytest1101@icloud.com>
2019-10-29 21:40:52 -0500

    Production Ready

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Loader from './Components/Loader';
 import {isObject, get} from 'lodash';
 import moment from 'moment';
 import MoneyFormation from "./Helpers/MoneyFormation";
-import Submission from './Data/FakeSubmission';
+// import Submission from './Data/FakeSubmission';
 
 // directives
 import './Directives/V-UIkit/Form';
@@ -564,8 +564,8 @@ let waitForElement = setInterval(() => {
         return h(App);
       },
       mounted() {
-        return this.setSubmission(Submission)
-          .then(() => this.$nextTick(() => this.loading = false));
+        /*return this.setSubmission(Submission)
+          .then(() => this.$nextTick(() => this.loading = false));*/
         if (this.token !== '') {
           getToken(this.origin, {token: this.token})
             .then(data => {
